051b9b3db072f2ff09ef1146e94fd6d381b39631
MacBook Air <macbookair@ringgit.me>
2024-06-27 17:53:34 +0800

    CowsAndBulls Number Game

--- CowsAndBulls/ContentView.swift
+++ CowsAndBulls/ContentView.swift
@@ -9,15 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        let guesses = Array(repeating: "1234", count: 20)
+        
+        VStack(spacing: 0) {
+            HStack {
+                TextField("Enter a guess...", text: .constant("1234"))
+                Button("Go", action: submitGuess)
+            }
+            .padding()
+            List(guesses, id: \.self) {guess in
+                HStack {
+                    Text(guess)
+                    Spacer()
+                    Text(result(for: guess))
+                }
+            }
         }
-        .padding()
+        .frame(width: 250)
+        .frame(minHeight: 300, maxHeight: .infinity)
+    }
+    
+    func submitGuess() {
+        
+    }
+    
+    func result(for guess: String) -> String {
+        "Result"
     }
 }
+
 
 #Preview {
     ContentView()
