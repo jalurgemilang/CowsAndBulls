61b880c8a315e4f37e56a9269f5747fc69d35e70
MacBook Air <macbookair@ringgit.me>
2024-06-27 17:17:17 +0800

    Initial Commit

---
+++ CowsAndBulls/CowsAndBullsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CowsAndBullsApp.swift
+//  CowsAndBulls
+//
+//  Created by Long Fong Yee on 27/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CowsAndBullsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
